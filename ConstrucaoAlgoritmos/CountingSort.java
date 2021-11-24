9568ba0c40593d1053bfeed401c8e13e9903b14a
brennoharten <brennoharten@gmail.com>
2021-11-23 23:17:51 -0300

    Finalizado passo 3 do lab 1 de analise de algoritm

--- ConstrucaoAlgoritmos/CountingSort.java
+++ ConstrucaoAlgoritmos/CountingSort.java
@@ -29,13 +29,13 @@ public class CountingSort {
 		for (int i = 0; i < alunos.length; i++) {
 			alunos[i] = new Aluno();
 		}
-		imprime(alunos);
-		alunos = ordenaCreditos(alunos);
-		imprime(alunos);
+		//imprime(alunos);
+		//alunos = ordenaCreditos(alunos);
+		// imprime(alunos);
 		// alunos = ordenaMatricula(alunos);
-		// imprime(alunos);
-		// alunos = ordenaNome(alunos);
-		// imprime(alunos);
+		imprime(alunos);
+		alunos = ordenaNome(alunos);
+		imprime(alunos);
 	}
 	
 	static Aluno[] ordenaCreditos (Aluno[] A) {
@@ -43,7 +43,7 @@ public class CountingSort {
         int[] C = new int[120];
 
         Arrays.fill(C ,0);
-
+		
         for (int j = 0; j < A.length; j++) {
             C[A[j].creditos] = C[A[j].creditos] + 1;
         }
@@ -60,12 +60,55 @@ public class CountingSort {
 	}
 
 	static Aluno[] ordenaMatricula (Aluno[] A) {
-		return A;
+		Aluno[] B = new Aluno[A.length];
+		int[] C = new int[10];
+		
+		// int placeValue = 1;
+		for (int d = 8; d >= 0; d--){
+			for (int j = 0; j < A.length; j++) {
+				int digito = (int) A[j].matricula.charAt(d) - '0';
+				C[digito] = C[digito]+1;
+			}
+			for (int i = 1; i < C.length; i++) {
+				C[i] = C[i-1] + C[i];
+			}
+			for (int i = A.length -1; i >= 0; i--) {
+				int digito = (int) A[i].matricula.charAt(d) - '0';
+				B[C[digito]-1] = A[i];
+				C[digito]--;
+			}
+			A = B.clone();
+			Arrays.fill(C ,0);
+		}
+		return B;
 	}
 
+	
 	static Aluno[] ordenaNome (Aluno[] A) {
-		return A;
+		Aluno[] B = new Aluno[A.length];
+		int[] C = new int[25];
+		
+		// int placeValue = 1;
+		for (int d = 7; d >= 0; d--){
+			for (int j = 0; j < A.length; j++) {
+				int digito = (int) A[j].nome.toLowerCase().charAt(d) - 'a';
+				C[digito] = C[digito]+1;
+			}
+			for (int i = 1; i < C.length; i++) {
+				C[i] = C[i-1] + C[i];
+			}
+			for (int i = A.length -1; i >= 0; i--) {
+				int digito = (int) A[i].nome.toLowerCase().charAt(d) - 'a';
+				B[C[digito]-1] = A[i];
+				C[digito]--;
+			}
+			A = B.clone();
+			Arrays.fill(C ,0);
+		}
+
+		return B;
 	}
+
 
 	static void imprime (Aluno[] A) {
 		for (int i = 0; i < A.length; i++) {
