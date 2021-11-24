a990b5dde1e2acaad92e769d737b056eb1a047d3
brennoharten <brennoharten@gmail.com>
2021-11-24 14:01:00 -0300

    finalizado lab01 av3 de algoritmos

--- ConstrucaoAlgoritmos/CountingSort.java
+++ ConstrucaoAlgoritmos/CountingSort.java
@@ -29,18 +29,18 @@ public class CountingSort {
 		for (int i = 0; i < alunos.length; i++) {
 			alunos[i] = new Aluno();
 		}
-		imprime(alunos);
+		/* imprime(alunos);
 		alunos = ordenaCreditos(alunos);
-		imprime(alunos);
+		imprime(alunos); */
 		// alunos = ordenaMatricula(alunos);
-		// imprime(alunos);
-		// alunos = ordenaNome(alunos);
-		// imprime(alunos);
+		imprime(alunos);
+		alunos = ordenaNome(alunos);
+		imprime(alunos);
 	}
 	
 	static Aluno[] ordenaCreditos (Aluno[] A) {
 		Aluno[] B = new Aluno[A.length];
-        int[] C = new int[120];
+        int[] C = new int[121];
 
         Arrays.fill(C ,0);
 
@@ -60,13 +60,55 @@ public class CountingSort {
 	}
 
 	static Aluno[] ordenaMatricula (Aluno[] A) {
-		return A;
+		Aluno[] B = new Aluno[A.length];
+		int[] C = new int[10];
+		
+		// int placeValue = 1;
+		for (int d = 8; d >= 0; d--){
+			for (int j = 0; j < A.length; j++) {
+				int digito = (int) A[j].matricula.charAt(d) - '0';
+				C[digito] = C[digito]+1;
+			}
+			for (int i = 1; i < C.length; i++) {
+				C[i] = C[i-1] + C[i];
+			}
+			for (int i = A.length -1; i >= 0; i--) {
+				int digito = (int) A[i].matricula.charAt(d) - '0';
+				B[C[digito]-1] = A[i];
+				C[digito]--;
+			}
+			A = B.clone();
+			Arrays.fill(C ,0);
+		}
+		return B;
 	}
 
+	
 	static Aluno[] ordenaNome (Aluno[] A) {
-		return A;
-	}
+		Aluno[] B = new Aluno[A.length];
+		int[] C = new int[26];
+		
+		// int placeValue = 1;
+		for (int d = 7; d >= 0; d--){
+			for (int j = 0; j < A.length; j++) {
+				int digito = (int) A[j].nome.toLowerCase().charAt(d) - 'a';
+				C[digito] = C[digito]+1;
+			}
+			for (int i = 1; i < C.length; i++) {
+				C[i] = C[i-1] + C[i];
+			}
+			for (int i = A.length -1; i >= 0; i--) {
+				int digito = (int) A[i].nome.toLowerCase().charAt(d) - 'a';
+				B[C[digito]-1] = A[i];
+				C[digito]--;
+			}
+			A = B.clone();
+			Arrays.fill(C ,0);
+		}
 
+		return B;
+	}
+	
 	static void imprime (Aluno[] A) {
 		for (int i = 0; i < A.length; i++) {
 			System.out.printf("%-15s%-15s%10d\n", A[i].matricula, A[i].nome, A[i].creditos);
